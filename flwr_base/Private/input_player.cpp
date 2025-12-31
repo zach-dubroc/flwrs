b54e11d822dfa0e51933117203abc867a22a58cf
Zachary Dubroc <Zachary.Dubroc@mscoding.org>
2025-12-31 09:49:49 -0600

    cam/sprint

--- flwr_base/Private/input_player.cpp
+++ flwr_base/Private/input_player.cpp
@@ -4,7 +4,9 @@
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "Camera/CameraComponent.h"
+#include "Components/SceneComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 Ainput_player::Ainput_player()
 {
@@ -15,38 +17,41 @@ Ainput_player::Ainput_player()
 	SpringArm->bUsePawnControlRotation = true;
 
 	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
-	fp_Camera = CreateDefaultSubobject<UCameraComponent>("fp_Camera");
 	Camera->SetupAttachment(SpringArm);
-	//TODO: attach to socket/adjust the clipping
-	fp_Camera->SetupAttachment(RootComponent);
+
+	//fp_CameraPivot = CreateDefaultSubobject<USceneComponent>("fp_CameraPivot");
+	//fp_CameraPivot->SetupAttachment(GetMesh(), fp_CameraSocket);
+
+	fp_Camera = CreateDefaultSubobject<UCameraComponent>("fp_Camera");
+	fp_Camera->SetupAttachment(GetMesh(),fp_CameraSocket);
 	fp_Camera->bUsePawnControlRotation = true;
+
+	walk_speed = FMath::Clamp(walk_speed, 150.f, 1200.f);
+	sprint_speed = FMath::Clamp(sprint_speed, walk_speed + 1.f, 2400.f);
 }
 void Ainput_player::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 void Ainput_player::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 void Ainput_player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 	if (APlayerController* PlayerController = Cast<APlayerController>(Controller)) {
 		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) {
 			Subsystem->AddMappingContext(input_mapping, 0);
 		}
 	}
-
 	if (UEnhancedInputComponent* input = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
 		input->BindAction(move_action, ETriggerEvent::Triggered, this, &Ainput_player::Move);
 		input->BindAction(look_action, ETriggerEvent::Triggered, this, &Ainput_player::Look);
 		input->BindAction(jump_action, ETriggerEvent::Triggered, this, &Ainput_player::Jump);
 		input->BindAction(toggle_cam_action, ETriggerEvent::Triggered, this, &Ainput_player::ToggleCam);
-
+		input->BindAction(sprint_action, ETriggerEvent::Started, this, &Ainput_player::SprintStart);
+		input->BindAction(sprint_action, ETriggerEvent::Completed, this, &Ainput_player::SprintStop);
 	}
 }
 void Ainput_player::Move(const FInputActionValue& input_value)
@@ -65,7 +70,6 @@ void Ainput_player::Move(const FInputActionValue& input_value)
 
 	}
 }
-
 void Ainput_player::Look(const FInputActionValue& input_value)
 {
 	FVector2D input_vector = input_value.Get<FVector2D>();
@@ -79,23 +83,33 @@ void Ainput_player::ToggleCam()
 {
 	if (IsValid(Controller)) {
 		//if clipping can use GetMesh->SetOwnerNoSee
+		//better fix maybe is attaching fp_camera to head socket and using eye offset
 		is_first_person = !is_first_person;
 		if (is_first_person) {
 			fp_Camera->SetActive(true);
 			Camera->SetActive(false);
-			// GetMesh()->SetOwnerNoSee(true);
+			//GetMesh()->SetOwnerNoSee(true);
+
 		}
 		else {
 			fp_Camera->SetActive(false);
 			Camera->SetActive(true);
-			// GetMesh()->SetOwnerNoSee(false);
+			//GetMesh()->SetOwnerNoSee(false);
 		}
 	}
 }
-
 void Ainput_player::Jump()
 {
 	ACharacter::Jump();
 }
+void Ainput_player::SprintStart(const FInputActionValue& input_value)
+{
+	GetCharacterMovement()->MaxWalkSpeed = sprint_speed;
+}
+void Ainput_player::SprintStop(const FInputActionValue& input_value)
+{
+	GetCharacterMovement()->MaxWalkSpeed = walk_speed;
+}
+
 
 
--- flwr_base/Public/input_player.h
+++ flwr_base/Public/input_player.h
@@ -20,25 +20,42 @@ class FLWR_BASE_API Ainput_player : public ACharacter
 	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* fp_Camera;
 
-	UPROPERTY(VisibleAnywhere, Category="Camera")
-	bool is_first_person = false;
 
 protected:
 	UPROPERTY(EditAnywhere, Category="EnhancedInput")
 	class UInputMappingContext* input_mapping;
-
 	UPROPERTY(EditAnywhere, Category="EnhancedInput")
 	class UInputAction* move_action;
-
 	UPROPERTY(EditAnywhere, Category="EnhancedInput")
 	class UInputAction* look_action;
-
 	UPROPERTY(EditAnywhere, Category="EnhancedInput")
 	class UInputAction* jump_action;
-
+	UPROPERTY(EditAnywhere, Category="EnhancedInput")
+	class UInputAction* sprint_action;
 	UPROPERTY(EditAnywhere, Category="EnhancedInput")
 	class UInputAction* toggle_cam_action;
 		
+	UPROPERTY(VisibleAnywhere, Category="Camera")
+	bool is_first_person = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	USceneComponent* fp_CameraPivot;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera", meta = (AllowPrivateAccess = "true"))
+	FName fp_CameraSocket = "head";
+
+
+	UPROPERTY(EditDefaultsOnly, Category="Camera")
+	float camera_blend_time = 0.5f;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool is_sprinting = false;
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float walk_speed = 600.f;
+	UPROPERTY(EditDefaultsOnly, Category = "Movement")
+	float sprint_speed = 1200.f;
+
 
 public:
 	Ainput_player();
@@ -55,4 +72,6 @@ protected:
 	void Look(const FInputActionValue& input_value);
 	void ToggleCam();
 	void Jump();
+	void SprintStop(const FInputActionValue& input_value);
+	void SprintStart(const FInputActionValue& input_value);
 };
